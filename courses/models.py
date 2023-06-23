a9766383a9161d8a3dd5f5f1f853573457720483
Ahmet Ertürk <is_ahmet@outlook.com>
2023-06-23 18:15:44 +0200

    Home page dynamisch

--- courses/models.py
+++ courses/models.py
@@ -7,10 +7,21 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    slug = models.SlugField(max_length=50, unique=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
 
 class Course(models.Model):
     name = models.CharField(max_length=200, unique=True)
     category = models.ForeignKey(Category, null=True, on_delete=models.DO_NOTHING)
+    tags = models.ManyToManyField(Tag, null=True, blank=True)
     description = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to="courses/%Y/%m/%d/", default="media\courses\2023\06\19\default.png")
     date = models.DateTimeField(auto_now=True)
--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from pages import views
+from pages.views import AboutView, IndexView
 
 urlpatterns = [
-    path('', views.index,name='index'),
-    path('about/', views.index,name='about'),
+    path('', IndexView.as_view(),name='index'),
+    path('about/', AboutView.as_view(),name='about'),
 ]
