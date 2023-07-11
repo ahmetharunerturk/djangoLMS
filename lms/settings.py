a326c0e2b26bd8239775be478d20aa3610683d68
Ahmet Ertürk <is_ahmet@outlook.com>
2023-07-11 15:33:52 +0200

    Vercel deployment

--- lms/settings.py
+++ lms/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-s-!=g843!-t-h%r7$+_+ntma8*8@1*&wyk6ksf*=bmhq1d+@+o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'lms.wsgi.application'
+WSGI_APPLICATION = 'vercel_app.wsgi.app'
 
 
 # Database
